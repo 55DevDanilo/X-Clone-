722178325e9e0bfa2dbb6aa609893a4f5433b5dc
55DevDanilo <dev.danilo_asl@outlook.com>
2025-03-22 19:04:00 -0300

    Pesquisando por outros usuários

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -14,34 +14,34 @@ class AppController extends Action
     {
 
 
-            $this->validaAutenticacao();
+        $this->validaAutenticacao();
 
-            $tweet = Container::getModel('Tweet');
+        $tweet = Container::getModel('Tweet');
 
-            $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
 
-            $tweets = $tweet->getAll();
-            // echo '<pre>';
+        $tweets = $tweet->getAll();
+        // echo '<pre>';
 
-            // print_r($tweets);
+        // print_r($tweets);
 
-            // echo '</pre>';
+        // echo '</pre>';
 
-            $this->view->tweets = $tweets;
+        $this->view->tweets = $tweets;
 
-            $this->render('timeline');
-            # code...
+        $this->render('timeline');
+        # code...
 
 
-        
+
     }
 
     public function tweet()
     {
 
-      //session_start();
+        //session_start();
         $this->validaAutenticacao();
-       
+
         //  $this->render('timeline');
         //  print_r($_POST);
 
@@ -50,18 +50,41 @@ class AppController extends Action
         $tweet->__set('id_usuario', $_SESSION['id']);
         $tweet->salvar();
         header('Location: /timeline');
-      
     }
 
     public function validaAutenticacao()
     {
         session_start();
 
-        if (!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome'])|| $_SESSION['nome']=='' ) {
+        if (!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == '') {
             header('Location: /?login=erro');
-        } 
-        else {
-            
+        } else {
         }
+    }
+
+    public function quemSeguir()
+    {
+
+        $this->validaAutenticacao();
+
+        // echo '<br /><br /><br /><br /><br /><br />';
+
+        $pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+
+        $usuarios = array();
+        // echo 'Pesquisador por :' . $pesquisarPor;
+
+        if ($pesquisarPor != '') {
+
+            $usuario = Container::getModel('Usuario');
+            $usuario->__set('nome', $pesquisarPor);
+            $usuarios = $usuario->getAll();
+            print_r($usuarios);
+        }
+
+        $this->view->usuarios=$usuarios;
+
+
+        $this->render('quemSeguir');
     }
 }
